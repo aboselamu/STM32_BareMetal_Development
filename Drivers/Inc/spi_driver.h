1e9076d909478dc22467529b560a0c22582fd2d0
sELAMU <aboselamu@gmail.com>
2026-05-05 00:35:27 +0200

    Standalone LED Dimmer project with 4-layer architecture

---
+++ Drivers/Inc/spi_driver.h
@@ -0,0 +1,49 @@
+#ifndef SPI_DRIVER_H
+#define SPI_DRIVER_H
+
+#include "stm32f4xx.h" 
+
+/* ================= TYPES ================= */
+typedef enum {
+    SPI_READY = 0,
+    SPI_BUSY_TX_RX,
+    SPI_ERROR_TIMEOUT,
+    SPI_ERROR_OVR,
+    SPI_ERROR_MODF
+} SPI_State_t;
+
+typedef struct {
+    uint8_t mode;
+    uint8_t bus_config;
+    uint8_t prescaler;
+    uint8_t data_size;
+    uint8_t cpol;
+    uint8_t cpha;
+    uint8_t nss;
+} SPI_Config_t;
+
+typedef struct SPI_Handle_s {
+    SPI_TypeDef *Instance;   /* Register base address (e.g., SPI1) */[cite: 11]
+    SPI_Config_t Config;
+    uint8_t *pTxBuffer;
+    uint8_t *pRxBuffer;
+    uint32_t TxLen;
+    uint32_t RxLen;
+    uint32_t TxCount;
+    uint32_t RxCount;
+    SPI_State_t State;
+
+    /* Callbacks */
+    void (*TxRxCpltCallback)(struct SPI_Handle_s *hspi);[cite: 11]
+    void (*ErrorCallback)(struct SPI_Handle_s *hspi);  [cite: 11]
+} SPI_Handle_t;
+
+/* ================= API ================= */
+void SPI_Init(SPI_Handle_t *hspi);
+uint8_t SPI_Transfer_IT(SPI_Handle_t *hspi, uint8_t *tx, uint8_t *rx, uint32_t len);[cite: 11]
+void SPI_IRQHandling(SPI_Handle_t *hspi);
+
+/* MSP Hook - To be implemented in board.c */
+void SPI_MspInit(SPI_Handle_t *hspi);
+
+#endif
---
+++ System/inc/stm32_it.h
@@ -0,0 +1,8 @@
+#ifndef STM32_IT_H
+#define STM32_IT_H
+
+void USART2_IRQHandler(void);
+void EXTI0_IRQHandler(void);
+void EXTI9_5_IRQHandler(void);
+
+#endif
